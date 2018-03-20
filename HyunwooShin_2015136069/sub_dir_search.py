97032a1aceac2de36bc818e2d18539a4cfe0676c
DESKTOP-JVOPVUC\Administrator <sisn0596@gmail.com>
2018-03-20 22:12:10 +0900

    ..
    ..

---
+++ HyunwooShin_2015136069/sub_dir_search.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+import os
+
+def search(dirname):
+    filenames = os.listdir(dirname)
+    for filename in filenames:
+        full_filename = os.path.join(dirname, filename)
+        print (full_filename)
+
+search("c:/")
